b36c177cf6b5bc5eb9cd5b8f1dc3fbac57ab8038
Vanda Adams <vandamadams@gmail.com>
2020-08-10 21:04:26 +0100

    carousel styling

--- js/main.js
+++ js/main.js
@@ -36,7 +36,9 @@ inView('.section')
 $(function() {
   $('.multiple-items').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
 });
