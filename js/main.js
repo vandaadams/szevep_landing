645ab03e87a5d74cad3575e646dc26891e2cd1b9
Vanda Adams <vandamadams@gmail.com>
2020-08-12 11:59:33 +0100

    small screen styling

--- js/main.js
+++ js/main.js
@@ -45,8 +45,16 @@ $(function() {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
       }
     },
     {
