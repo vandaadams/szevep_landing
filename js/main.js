3fa2a65fdd5d0cdce0ce284aebb9713d62735706
Vanda Adams <vandamadams@gmail.com>
2020-08-12 11:16:16 +0100

    slider responsive js

--- js/main.js
+++ js/main.js
@@ -40,5 +40,22 @@ $(function() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
   });
 });
